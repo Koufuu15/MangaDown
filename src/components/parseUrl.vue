5d77cbef3429d740a5f4e8b9906bdcbce7261a2a
Koufu <nadeshiko20091229@gmail.com>
2026-06-16 11:50:51 +0900

    大まかなフロントが完成

--- src/components/parseUrl.vue
+++ src/components/parseUrl.vue
@@ -1,3 +1,43 @@
 <template>
-  <input type="url" placeholder="URLを入力" />
+  <label for="url">WebサイトのURLを入力</label><br>
+  <input id="url" type="url" placeholder="URLを入力" v-model="url"/>
+  <input type="button" value="読み込む" @click="parseUrl"/>
+
+  <div class="manga">
+    <p v-if="isLoading">Thinking...</p>
+    <p v-else-if="!url">URLを入力してください</p>
+    <p v-else-if="resMd">{{ resMd }}</p>
+    <p v-else-if="!res">データの取得に失敗しました</p>
+  </div>
+  <div class="manga">
+    <p v-if="resImg" v-html="resImg"></p>
+    <p v-else>プレビュー</p>
+  </div>
+
+  <input type="button" value="エディタで編集" @click="$router.push('/writeMD')"/>
+  <input type="button" value="このまま保存" @click="$router.push('/')"/>
 </template>
+
+<script setup>
+import { ref } from 'vue'
+
+const url = ref('')
+const resMd = ref('')
+const resImg = ref('')
+const isLoading = ref(false)
+function parseUrl() {
+  /*
+  ここでURLを解析して、生成したMDをresに格納する処理を書く
+  if (urlが正しい) 
+    url=取得したURL
+    AIにレスポンスを投げる
+    isLoading=true
+    if (AIからレスポンスが返ってきたら)
+      isLoading=false
+      resMd=AIからのレスポンス
+      resImg=AIからのレスポンスをhtmlに変換したもの
+  else url=''
+  */
+}
+
+</script>
