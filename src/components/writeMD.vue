81715b14cede1daddf8aedee17a3b223b6f4be40
Koufu <nadeshiko20091229@gmail.com>
2026-06-21 00:22:53 +0900

    CodeMirrorを追加

--- src/components/writeMD.vue
+++ src/components/writeMD.vue
@@ -1,14 +1,34 @@
 <script setup>
 import '../assets/main.css'
 import { useRouter } from 'vue-router'
+import { ref, onMounted } from 'vue'
+
+import { EditorView, basicSetup } from 'codemirror'
+import { markdown } from '@codemirror/lang-markdown'
+import { languages } from '@codemirror/language-data'
+
 const router = useRouter()
+const editor = ref(null)
+
+onMounted(() => {
+  new EditorView({
+    // TODO: what's doc?
+    doc: "console.log('Hello, world!')",
+    extensions: [
+        basicSetup, 
+        markdown({ codeLanguages: languages })
+      ],
+    parent: editor.value,
+  })
+})
+
 </script>
 
 <template>
   <input type="button" value="このまま公開" @click="router.push('/save')" />
   <div class="flexbox">
     <div class="manga">
-      <textarea placeholder="独自のMarkDownを入力" v-model="dataMd" > {{ dataMd }} </textarea>
+      <div ref="editor">{{ dataMd || "独自Markdownを入力してください。" }}</div>
     </div>
     <div class="manga">
       <p v-if="dataMd" v-html="dataMd"></p>
