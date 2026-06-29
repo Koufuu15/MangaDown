4aada0eb27516e99675f1d68ce8b9cef6fc6be81
Koufu <nadeshiko20091229@gmail.com>
2026-06-30 08:37:19 +0900

    キャラクターと吹き出しを追加

---
+++ src/components/manga/Bubble.vue
@@ -0,0 +1,24 @@
+<script setup>
+defineProps({
+  bubble: {
+    type: Object,
+    required: true
+  }
+})
+</script>
+
+<template>
+  <div
+    :style="{
+      position: 'absolute',
+      left: `${bubble.position.x}%`,
+      top: `${bubble.position.y}%`,
+      border: '1px solid black',
+      backgroundColor: 'white',
+      padding: '8px',
+      fontSize: '14px'
+    }"
+  >
+    {{ bubble.text }}
+  </div>
+</template>
--- src/components/manga/Panel.vue
+++ src/components/manga/Panel.vue
@@ -1,4 +1,6 @@
 <script setup>
+import Bubble from "./Bubble"
+
 defineProps({
   panel: {
     type: Object,
@@ -16,6 +18,31 @@ defineProps({
       backgroundColor: panel.backgroundColor || '#eee'
     }"
   />
+  <div
+    v-for="(layer, index) in panel.layers"
+    :key="index"
+  >
+    <!-- Character -->
+    <img
+      v-if="layer.type === 'character'"
+      :src="characterAssets[`${layer.name}_${layer.pose}`]"
+      :alt="layer.name"
+      :style="{
+        position: 'absolute',
+        left: `${layer.position.x}%`,
+        top: `${layer.position.y}%`,
+        width: `${layer.size?.w || 80}px`,
+        height: `${layer.size?.h || 120}px`,
+        objectFit: 'contain'
+      }"
+    />
+
+    <!-- Bubble -->
+    <Bubble
+      v-else-if="layer.type === 'bubble'"
+      :layer="layer"
+    />
+  </div>
 </template>
 
 <style scoped>
